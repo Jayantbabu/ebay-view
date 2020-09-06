ca2275f7607df4d72e3ad5fe53d5ac77318d6a45
Jayant Babu <jayantbabu@MacBook-Pro.local>
2020-09-06 08:55:50 +0530

    update

--- ebay-view.py
+++ ebay-view.py
@@ -8,21 +8,23 @@ from linkpreview import link_preview
 bot = commands.Bot(command_prefix="-", description="ebay-view-bot", case_insensitive=True)
 
 log_channel_id = 751991759025864794
-
+bot.footer_image = "https://1000logos.net/wp-content/uploads/2018/08/eBay-Logo.png"
 bot.footer = "ebay-view-bot"
 
 @bot.command(aliases=['ebay'], help = "Give the link and number of views")
-async def ebay_view(ctx, link, number):
+async def view(ctx, link, number):
 	embed = discord.Embed(
-				title = f'Giving {number} views to the {link}',
+				title = f'Gave {number} views to the {link}',
 				color = discord.Color.from_rgb(68, 128, 123),
 				timestamp = datetime.datetime.now(datetime.timezone.utc)
 				 )
-
-	embed.set_footer(icon_url = link_preview(link, requests.get(link).content.decode("utf-8")).image)
+	bot.foot = link_preview(link, requests.get(link).content.decode("utf-8")).image
+	embed.set_image(url=bot.foot)
 	bot.log_channel = bot.get_channel(log_channel_id)
-	for num in range(number):
-		await requests.get(link)
+	for num in range(int(number)):
+		requests.get(link)
+		print(num)
+	await bot.log_channel.send(embed = embed)
 
 
 @bot.event
@@ -36,11 +38,9 @@ async def on_ready():
 			color = discord.Color.from_rgb(68, 128, 123),
 			timestamp = datetime.datetime.now(datetime.timezone.utc)
 			)
-	embed.set_footer(
-			icon_url = "https://pbs.twimg.com/media/EVmo6vpWsAQxl3j.jpg"
-			)
+	embed.set_image(url=bot.footer_image)
 	bot.log_channel = bot.get_channel(log_channel_id)
 	await bot.log_channel.send(embed = embed)
 
 if __name__ == "__main__":
-		bot.run('NzUxOTY3Nzk0MjkxMDE1Nzcy.X1QylQ.12uwrSqef_Vllv5ZGHZ39Tc-p-M',  reconnect=True)
+		bot.run('NzUxOTY3Nzk0MjkxMDE1Nzcy.X1QylQ.12uwrSqef_Vllv5ZGHZ39Tc-p-M', bot=True, reconnect=True)
